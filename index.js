a13f76cc66abd548bd6732bc81c8fffc6cf0d706
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-03-06 11:54:40 +0100

    nu virker det med tiden men knapperne virker ikke endnu

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    let pauses = [
+        {pauseStart:"09:40", pauseSlut:"10:00", besked:"Pause"},
+        {pauseStart:"11:30", pauseSlut:"12:00", besked:"Pause"},
+        {pauseStart:"13:30", pauseSlut:"13:40", besked:"Pause"}
+    ];
+    let body = document.querySelector("body");
+    //changes the time into one number - easier to manage
+    function timeInOneNbr(hours, minutes){
+        return parseInt(hours) * 60 + parseInt(minutes) 
+    }
+
+    //getting the time every 500 ms
+    setInterval(() => {
+        let today = new Date();
+        let time = today.getHours() + ":" + today.getMinutes();
+        let currentTime = timeInOneNbr(today.getHours(), today.getMinutes());
+        let message = "Skoletid"; 
+
+        document.querySelector(".currentTime").innerHTML =  "Klokken er " + time; 
+        
+    for (const pause of pauses) { 
+        let start = pause.pauseStart.split(":");
+        let startMinutes = timeInOneNbr(...start);
+        let end = pause.pauseSlut.split(":");
+        let endMinutes = timeInOneNbr(...end);
+
+        //checks for a break
+        if(currentTime >= startMinutes && currentTime <= endMinutes){ 
+            message = pause.besked;
+                break;
+        }
+    }
+    //changes the message when you are outside school time(weekends and time when your done for the day)
+    let day = today.getDate();
+    if(day === 0 || day === 1){
+        message = "Det er weekend"
+    }else if(day === 2 || day === 6){
+        if(time >= "13:30"){
+            message = "Færdig for i dag"
+        }
+    }else{
+        if(time >= "15:10"){
+            message = "Færdig for i dag"
+        }  
+    }
+
+    //shows the relevant message
+    document.querySelector(".message").innerHTML = message;
+
+    //tells the light what color to be based on the message
+    if(message === "Skoletid"){
+        body.style.backgroundColor="#1414cc";
+        fetching(46920, 84, true);
+    }else if(message === "Pause"){
+        body.style.backgroundColor="#d11f0b";
+        fetching(65535, 80, true);
+    }else{
+        body.style.backgroundColor="#000";
+        fetching(56656, 90, false);
+    }
+    }, 500);
+
     //----------------------------henter fra APIet------------------------------
-    function fetching(color, brightness){
+    function fetching(color, brightness, onOff){
         fetch("https://192.168.8.100/api/90pkxK64tycrPfvs7qn0n-mgoLHMG9hm0W6QY54G/lights/12/state", {
             method: "PUT",
             header: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                on:true,
+                on:onOff,
                 hue:color,
                 sat:245,
                 bri:brightness
@@ -19,53 +81,3 @@ document.addEventListener("DOMContentLoaded", ()=>{
         })
         .catch(error => console.error(error))
     }
-    function fetchOff(){
-        fetch("https://192.168.8.100/api/90pkxK64tycrPfvs7qn0n-mgoLHMG9hm0W6QY54G/lights/12/state/", {
-            method: "PUT",
-            header: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                on:false
-            })
-        })
-        .then(response => response.json())
-        .then(function(result){
-            console.log(result)
-        })
-        .catch(error => console.error(error))
-    }
-
-    //----------------------------buttons---------------------------------------
-    let body = document.querySelector("body");
-    let Skoletid = document.querySelector(".Skoletid");
-    let Pause = document.querySelector(".Pause");
-    let Oplæg = document.querySelector(".Oplæg");
-    let CodeAlong = document.querySelector(".Code-Along");
-    let Færdig = document.querySelector(".Færdig");
-
-    Skoletid.addEventListener("click", ()=>{
-        body.style.backgroundColor="#1414cc";
-        fetching(46920, 84);
-    });
-
-    Pause.addEventListener("click", ()=>{
-        body.style.backgroundColor="#d11f0b";
-        fetching(65535, 80);
-    });
-    
-    Oplæg.addEventListener("click", ()=>{
-        body.style.backgroundColor="#ce09d1";
-        fetching(56656, 90);
-    });
-    
-    CodeAlong.addEventListener("click", ()=>{
-        body.style.backgroundColor="#f0c91d";
-        fetching(10000, 84);
-    });
-
-    Færdig.addEventListener("click", ()=>{
-        body.style.backgroundColor="#000";
-        fetchOff();
-    });
-});
