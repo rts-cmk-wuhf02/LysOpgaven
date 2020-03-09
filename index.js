8010f8baade5571b6e330da64d5b8dfc90b35326
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-03-09 10:38:12 +0100

    nu virker knapperne

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let pauses = [
-        {pauseStart:"09:40", pauseSlut:"10:00", besked:"Pause"},
-        {pauseStart:"11:30", pauseSlut:"12:00", besked:"Pause"},
-        {pauseStart:"13:30", pauseSlut:"13:40", besked:"Pause"}
+        {pauseStart:"8:10", pauseSlut:"9:40", besked:"Skoletid"},//ikke pause
+        {pauseStart:"9:40", pauseSlut:"10:0", besked:"Pause"},
+        {pauseStart:"10:0", pauseSlut:"11:30", besked:"Skoletid"},//ikke pause
+        {pauseStart:"11:30", pauseSlut:"12:0", besked:"Pause"},
+        {pauseStart:"12:0", pauseSlut:"13:30", besked:"Skoletid"},//ikke pause
+        {pauseStart:"13:30", pauseSlut:"13:40", besked:"Pause"},
+        {pauseStart:"13:40", pauseSlut:"15:10", besked:"Skoletid"}//ikke pause
     ];
+
     let body = document.querySelector("body");
     //changes the time into one number - easier to manage
     function timeInOneNbr(hours, minutes){
@@ -15,50 +20,73 @@ document.addEventListener("DOMContentLoaded", ()=>{
         let today = new Date();
         let time = today.getHours() + ":" + today.getMinutes();
         let currentTime = timeInOneNbr(today.getHours(), today.getMinutes());
-        let message = "Skoletid"; 
-
+        // let message = "Skoletid"; 
+        var message; 
         document.querySelector(".currentTime").innerHTML =  "Klokken er " + time; 
         
-    for (const pause of pauses) { 
-        let start = pause.pauseStart.split(":");
-        let startMinutes = timeInOneNbr(...start);
-        let end = pause.pauseSlut.split(":");
-        let endMinutes = timeInOneNbr(...end);
+        for (const pause of pauses) { 
+            let start = pause.pauseStart.split(":");
+            let startMinutes = timeInOneNbr(...start);
+            let end = pause.pauseSlut.split(":");
+            let endMinutes = timeInOneNbr(...end);
 
-        //checks for a break
-        if(currentTime >= startMinutes && currentTime <= endMinutes){ 
-            message = pause.besked;
-                break;
+            //checks for a break
+            if(currentTime >= startMinutes && currentTime <= endMinutes){ 
+                message = pause.besked;
+                    break;
+            }
         }
-    }
-    //changes the message when you are outside school time(weekends and time when your done for the day)
-    let day = today.getDate();
-    if(day === 0 || day === 1){
-        message = "Det er weekend"
-    }else if(day === 2 || day === 6){
-        if(time >= "13:30"){
+
+        //changes the message when you are outside school time(weekends and done for the day)
+        let day = today.getDay();
+        if(day === 1 && time >= "13:30" || day === 5 && time >= "13:30" ){
+        //1 = monday, 5 = friday
             message = "Færdig for i dag"
+        }else if(day >= 2 && day <= 4 && time >= "15:10"){
+            message = "Færdig for i dag"
+        }else if(day === 6 || day === 0){
+        //6 = saturday, 0 = sunday
+            message = "Det er weekend"
         }
-    }else{
-        if(time >= "15:10"){
-            message = "Færdig for i dag"
-        }  
-    }
 
-    //shows the relevant message
-    document.querySelector(".message").innerHTML = message;
+        document.querySelector(".Code-Along").addEventListener("click", ()=>{
+            if(OplægOn === true){
+                document.querySelector(".Oplæg").classList.remove("OplægOn");
+            }
+            document.querySelector(".Code-Along").classList.toggle("CodeAlongOn");
+        });
 
-    //tells the light what color to be based on the message
-    if(message === "Skoletid"){
-        body.style.backgroundColor="#1414cc";
-        fetching(46920, 84, true);
-    }else if(message === "Pause"){
-        body.style.backgroundColor="#d11f0b";
-        fetching(65535, 80, true);
-    }else{
-        body.style.backgroundColor="#000";
-        fetching(56656, 90, false);
-    }
+        var CodeAlongOn = document.querySelector(".Code-Along").classList.contains("CodeAlongOn");
+
+        document.querySelector(".Oplæg").addEventListener("click", ()=>{
+            if(CodeAlongOn === true){
+                document.querySelector(".Code-Along").classList.remove("CodeAlongOn");
+            }
+            document.querySelector(".Oplæg").classList.toggle("OplægOn");
+        });
+        
+        var OplægOn = document.querySelector(".Oplæg").classList.contains("OplægOn");
+
+        //tells the light what color to be based on the message
+        if(CodeAlongOn === true){
+            body.style.backgroundColor="#f0c91d";
+            fetching(10000, 84, true);
+        }else if(OplægOn === true){
+            body.style.backgroundColor="#ce09d1";
+            fetching(56656, 90, true);
+        }else if(message === "Skoletid"){
+            body.style.backgroundColor="#1414cc";
+            fetching(46920, 84, true);
+        }else if(message === "Pause"){
+            body.style.backgroundColor="#d11f0b";
+            fetching(65535, 80, true);
+        }else{
+            body.style.backgroundColor="#000";
+            fetching(56656, 90, false);
+        }
+    
+        //shows the relevant message
+        document.querySelector(".message").innerHTML = message;
     }, 500);
 
     //----------------------------henter fra APIet------------------------------
@@ -74,10 +102,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 sat:245,
                 bri:brightness
             })
-        })
-        .then(response => response.json())
-        .then(function(result){
-            console.log(result)
         })
         .catch(error => console.error(error))
     }
